13654beb633fddb167ae6b9a2e48028e6f1e8ca2
vhs-cake <vali.kravtsova@gmail.com>
2025-04-25 12:57:34 +0200

    func from utils added to home view model

--- app/src/main/java/cvut/zan/myimdb/ui/home/HomeViewModel.kt
+++ app/src/main/java/cvut/zan/myimdb/ui/home/HomeViewModel.kt
@@ -4,9 +4,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import cvut.zan.myimdb.movie.domain.models.Movie
 import cvut.zan.myimdb.movie.domain.repository.MovieRepository
+import cvut.zan.myimdb.utils.collectAndHandle
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -17,8 +19,48 @@ class HomeViewModel @Inject constructor(
     private val _homeState = MutableStateFlow(HomeState())
     val homeState = _homeState.asStateFlow()
 
+    init {
+        fetchDiscoverMovie()
+    }
+    init {
+        fetchTrendingMovie()
+    }
+
     private fun fetchDiscoverMovie() = viewModelScope.launch {
-        repository.fetchDiscoverMovie()
+        repository.fetchDiscoverMovie().collectAndHandle(
+            onError = { error ->
+                _homeState.update {
+                    it.copy(isLoading = false, error = error?.message)
+                }
+            },
+            onLoading = {
+                _homeState.update {
+                    it.copy(isLoading = true, error = null)
+                }
+            }
+        ) { movie ->
+            _homeState.update {
+                it.copy(isLoading = false, error = null, discoverMovies = movie)
+            }
+        }
+    }
+    private fun fetchTrendingMovie() = viewModelScope.launch {
+        repository.fetchDiscoverMovie().collectAndHandle(
+            onError = { error ->
+                _homeState.update {
+                    it.copy(isLoading = false, error = error?.message)
+                }
+            },
+            onLoading = {
+                _homeState.update {
+                    it.copy(isLoading = true, error = null)
+                }
+            }
+        ) { movie ->
+            _homeState.update {
+                it.copy(isLoading = false, error = null, trendingMovies = movie)
+            }
+        }
     }
 }
 
