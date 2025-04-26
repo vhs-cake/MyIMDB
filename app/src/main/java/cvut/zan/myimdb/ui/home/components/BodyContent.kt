52969e813c6915c6fae3752e9503c9e835e34d2b
vhs-cake <vali.kravtsova@gmail.com>
2025-04-26 02:00:09 +0200

    body content - lazy column row added

---
+++ app/src/main/java/cvut/zan/myimdb/ui/home/components/BodyContent.kt
@@ -0,0 +1,49 @@
+package cvut.zan.myimdb.ui.home.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import cvut.zan.myimdb.movie.domain.models.Movie
+import cvut.zan.myimdb.ui.home.itemSpacing
+
+@Composable
+fun BodyContent(
+    modifier: Modifier = Modifier,
+    discoverMovies: List<Movie>,
+    trendingMovies: List<Movie>,
+    onMovieClick: (id:Int) -> Unit
+) {
+    LazyColumn(modifier = modifier) {
+        item {
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(itemSpacing),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Discover Movies",
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.Bold,
+                )
+                IconButton(onClick = {/*TODO*/}) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowForward,
+                        contentDescription = "More discover movies"
+                    )
+                }
+            }
+        }
+    }
+}
