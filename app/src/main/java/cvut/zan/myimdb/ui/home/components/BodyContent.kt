257c8380a97267292e444675be95df54a17375f6
vhs-cake <vali.kravtsova@gmail.com>
2025-04-26 02:27:59 +0200

    lazy row finished

--- app/src/main/java/cvut/zan/myimdb/ui/home/components/BodyContent.kt
+++ app/src/main/java/cvut/zan/myimdb/ui/home/components/BodyContent.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material3.Icon
@@ -47,7 +48,10 @@ fun BodyContent(
             }
             LazyRow {
                 items(discoverMovies) {
-
+                    MovieCoverImage(
+                        movie = it,
+                        onMovieClick = onMovieClick
+                    )
                 }
             }
         }
